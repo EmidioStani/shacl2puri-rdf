41f51b84ffb77681e509e095586995a1fab6af39
emidiostani <emidiostani@gmail.com>
2022-07-12 01:13:05 +0200

    fixed queries

--- shacl2puri-rdf.py
+++ shacl2puri-rdf.py
@@ -1,3 +1,5 @@
+import os
+from os import path
 from pathlib import Path
 from typing import Optional
 from unicodedata import decimal
@@ -14,17 +16,40 @@ def get_config(file):
         config = yaml.load(config_file, Loader=yaml.FullLoader)
     return config
 
+def read_files(graph, folder):
+    for filename in os.listdir(folder):
+        file_path = path.relpath(folder + "/" + filename)
+        # checking if it is a file
+        if os.path.isfile(file_path):
+            print("reading: " + file_path)
+            file = open(file_path, 'r')
+            # print(file.read())
+            graph.parse(source=file_path, format=config['input']['format'])
+            print(len(graph))
+            file.close()
+    return graph
+
 config = get_config("config.yaml")
 
 g = Graph()
-g.parse(config['input']['file'] , format=config['input']['format'])
+folder = config['input']['folder']
+g = read_files(g, folder)
+# print(len(g))
+#g.parse(config['input']['file'] , format=config['input']['format'])
 
 sparql_datatype_properties = """
     CONSTRUCT {
         ?path rdf:type rdf:Property .
         ?path rdf:type owl:DatatypeProperty .
         ?path rdfs:range ?type .
-        ?path rdfs:comment ?desc . 
+        ?path rdfs:comment ?desc .
+        ?path rdfs:label ?name .
+        ?path rdfs:isDefinedBy <http://data.europa.eu/m8g> .
+
+        ?class rdf:type owl:Class .
+        ?class rdfs:comment ?classdesc .
+        ?class rdfs:label ?classname .
+
     }
     WHERE {
         ?s rdf:type shacl:NodeShape .
@@ -32,15 +57,72 @@ sparql_datatype_properties = """
         ?p shacl:path ?path .
         ?p shacl:datatype ?type .
         ?p shacl:description ?desc .
-       FILTER (strstarts(str(?path), 'http://data.europa.eu/m8g/'))
+        ?p shacl:name ?name .
+
+        ?sclass rdf:type shacl:NodeShape .
+        ?sclass shacl:targetClass ?class .
+        OPTIONAL {?sclass shacl:description ?classdesc .} .
+        OPTIONAL {?sclass shacl:name ?classname} .
+
+        FILTER (strstarts(str(?path), 'http://data.europa.eu/m8g/'))
+        FILTER (strstarts(str(?class), 'http://data.europa.eu/m8g/'))
+
+
+    }
+"""
+
+sparql_object_properties = """
+    CONSTRUCT {
+        ?relpath rdf:type rdf:Property .
+        ?relpath rdf:type owl:ObjectProperty .
+        ?relpath rdfs:range ?relclass .
+        ?relpath rdfs:comment ?reldesc .
+        ?relpath rdfs:label ?relname .
+        ?relpath rdfs:isDefinedBy <http://data.europa.eu/m8g> .
+
+    }
+    WHERE {
+        ?sclass2 rdf:type shacl:NodeShape .
+        ?sclass2 shacl:property ?rel .
+        ?rel shacl:path ?relpath .
+        ?rel shacl:description ?reldesc .
+        ?rel shacl:name ?relname .
+        ?rel shacl:class ?relclass .
+        FILTER (strstarts(str(?relpath), 'http://data.europa.eu/m8g/'))
+
+    }
+"""
+
+sparql_classes = """
+    CONSTRUCT {
+        ?class rdf:type owl:Class .
+        ?class rdfs:comment ?classdesc .
+        ?class rdfs:label ?classname .
+
+    }
+    WHERE {
+        ?sclass rdf:type shacl:NodeShape .
+        ?sclass shacl:targetClass ?class .
+        OPTIONAL {?sclass shacl:description ?classdesc .} .
+        OPTIONAL {?sclass shacl:name ?classname} .
+        FILTER (strstarts(str(?class), 'http://data.europa.eu/m8g/'))
+
+
     }
 """
 
 qres = g.query(sparql_datatype_properties)
+qres2 = g.query(sparql_object_properties)
+qres3 = g.query(sparql_classes)
+
 goutput = Graph()
 for triple in qres:        
     goutput.add(triple)
-goutput.serialize(destination='output.txt', format='turtle')
+for triple in qres2:        
+    goutput.add(triple)
+for triple in qres3:        
+    goutput.add(triple)
+goutput.serialize(destination='output.ttl', format='turtle')
 
 sparql_select_subjects = """
     SELECT distinct ?s
@@ -60,6 +142,7 @@ for row in qsubject_list:
     for triple in qsamesubject:
         tempgraph.add(triple)
     filepath =  config['output']['folder'] + "/" + row[0].rsplit('/', 1)[-1]
+    print(filepath)
     tempgraph.serialize(destination= filepath + '.ttl', format='turtle')
     tempgraph.serialize(destination= filepath + '.nt', format='nt')
     tempgraph.serialize(destination= filepath + '.rdf', format='xml')
